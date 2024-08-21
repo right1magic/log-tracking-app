3ba4b8f0b814b378da1c1d80fe6f3dc7d56c2c30
right1magic <155404648+right1magic@users.noreply.github.com>
2024-08-21 17:04:02 +0900

    Create insert_log.sh

---
+++ insert_log.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# MySQL 접속 정보
+MYSQL_HOST="1bc8cc38-e15d-4308-9512-cf3a4cdf975a.internal.kr1.mysql.rds.nhncloudservice.comt"  # MySQL 서버의 엔드포인트 또는 IP 주소
+MYSQL_USER="log_admin"      # MySQL 사용자명
+MYSQL_PASSWORD="log_admin"  # MySQL 비밀번호
+MYSQL_DATABASE="log_backup"   # 스키마명 (데이터베이스 이름)
+MYSQL_TABLE="app_logs"      # 삽입할 테이블명
+
+# 로그 파일
+LOG_FILE="log.log"
+
+# 로그 파일을 한 줄씩 읽어서 처리
+while IFS=$',' read -r logtype logdate loguser logendpoint; do
+    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -P 3306 -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e \
+    "INSERT INTO $MYSQL_TABLE VALUES ('$logtype', '$logdate', '$loguser', '$logendpoint');"
+done < "$LOG_FILE"
