a8b9e5131ad9f744a9624cacb70d4d2303e999b0
right1magic <155404648+right1magic@users.noreply.github.com>
2024-08-22 11:35:12 +0900

    Update insert_log.sh

--- insert_log.sh
+++ insert_log.sh
@@ -8,11 +8,11 @@ MYSQL_DATABASE="log_backup"   # 스키마명 (데이터베이스 이름)
 MYSQL_TABLE="app_logs"      # 삽입할 테이블명
 
 # 로그 파일
-LOG_FILE="home/ubuntu/log.log"
+LOG_FILE="log.log"
 
 # 로그 파일을 한 줄씩 읽어서 처리
 while IFS=$',' read -r logtype logdate loguser logendpoint; do
     mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -P 3306 -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" -e \
-    "INSERT INTO $MYSQL_TABLE VALUES ('$id', '$log_type', '$log_timestamp', '$url', '$method', '$user_id', '$transaction_id',
+    "INSERT INTO $MYSQL_TABLE ( log_type, log_timestamp, url, method, user_id, transaction_id, product_id, cart_id, order_id, payload, client_ip, user_agent, referer) VALUES ( '$log_type', '$log_timestamp', '$url', '$method', '$user_id', '$transaction_id',
     '$product_id', '$cart_id', '$order_id', '$payload', '$client_ip', '$user_agent', '$referer');"
 done < "$LOG_FILE"
